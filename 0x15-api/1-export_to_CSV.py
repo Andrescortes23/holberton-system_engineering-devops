8e76be9d2d9dac77c6dc26219175cccec80c1ef3
Andrescortes23 <andresccortes23@icloud.com>
2021-02-02 03:57:52 +0000

    Task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Export to csv"""
+
+import requests
+from sys import argv
+import csv
+
+if __name__ == '__main__':
+    target = argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(target)).json()
+    todos = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos"
+                         .format(target)).json()
+
+    with open("{}.csv".format(target), 'w') as file:
+        writer = csv.writer(file, delimiter=',', quoting=csv.QUOTE_ALL)
+        for a in todos:
+            writer.writerow([a['userId'], user['username'],
+                             a['completed'], a['title']])
