7ad958023e8a860405a097cdb84e1db6c0652752
Andrescortes23 <andresccortes23@icloud.com>
2021-02-02 00:49:07 +0000

    Task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,14 +3,14 @@
 Write a Python script that, using this REST API, for a given employee ID,
 returns information about his/her TODO list progres
 """
-import requests
-from sys import argv
 
 if __name__ == "__main__":
+    import requests
+    from sys import argv
+
     employee_id = int(argv[1])
     todos = requests.get('https://jsonplaceholder.typicode.com/todos')
     todos = todos.json()
-    complet = 0
     totaltasks = 0
     titles = []
     for a in todos:
@@ -20,9 +20,8 @@ if __name__ == "__main__":
             username = user[employee_id - 1]['name']
             if a.get('completed') is True:
                 titles.append(a.get('title'))
-                complet += 1
             totaltasks += 1
-    print("Employee {} is done with tasks({}/{}):".format(username,
-                                                          complet, totaltasks))
+    print("Employee {} is done with tasks({}/{}):"
+          .format(username, len(titles), totaltasks))
     for ti in titles:
         print("\t {}".format(ti))
