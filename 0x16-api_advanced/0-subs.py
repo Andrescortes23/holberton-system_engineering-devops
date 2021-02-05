9cb8ed7b1e7fc1be3241872e9c8d35cecf7e9ab6
Andrescortes23 <andresccortes23@icloud.com>
2021-02-05 01:38:48 +0000

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ def number_of_subscribers(subreddit):
     try:
         url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
         url = requests.get(url, headers={'User-agent': 'your bot 0.1'},
-                       allow_redirects=False).json()
+                           allow_redirects=False).json()
         return url["data"].get("subscribers")
     except:
         return 0
