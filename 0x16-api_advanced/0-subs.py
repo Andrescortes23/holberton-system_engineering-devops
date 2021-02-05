5bd759fd8670c07b419b1ae6ed5261d7e89c331d
Andrescortes23 <andresccortes23@icloud.com>
2021-02-05 01:30:05 +0000

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,8 @@ import requests
 def number_of_subscribers(subreddit):
     """Return number of subs from subreddit"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    url = requests.get(url, headers={'User-agent': 'your bot 0.1'}).json()
+    url = requests.get(url, headers={'User-agent': 'your bot 0.1'},
+                       allow_redirects=False).json()
 
     if "subscribers" in url["data"]:
         return url["data"].get("subscribers")
