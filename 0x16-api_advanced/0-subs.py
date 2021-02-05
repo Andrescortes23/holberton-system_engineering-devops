a9b3bcd95467a77d5f4ac7c93c1efaa50ef847d1
Andrescortes23 <andresccortes23@icloud.com>
2021-02-05 00:58:51 +0000

    Task0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" To know number of subs"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Return number of subs from subreddit"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = requests.get(url, headers={'User-agent': 'your bot 0.1'}).json()
+
+    if url["data"].get("subscribers"):
+        return url["data"].get("subscribers")
+    else:
+        return 0
