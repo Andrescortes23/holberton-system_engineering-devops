d6ae160d65a1f3f3f56d95bf6cb6f1e8239bf9e4
Andrescortes23 <andresccortes23@icloud.com>
2021-02-18 21:23:57 +0000

    task 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # puppet script for code 500 correction
-exec {'replacing':
+exec { 'fixer':
      command  => 'sed -i "s|class-wp-locale.phpp|class-wp-locale.php|g" /var/www/html/wp-settings.php',
      provider => shell,
 }
