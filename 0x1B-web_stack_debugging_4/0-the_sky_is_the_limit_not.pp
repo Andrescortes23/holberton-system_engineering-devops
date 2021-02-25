8691dc593aeb24841e665f1d0a0c6daa8ef84982
Andrescortes23 <andresccortes23@icloud.com>
2021-02-25 05:32:38 +0000

    Task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # debbug to set number of request to unlimit
-exec { 'upperlimit' :
+exec { 'unlock' :
   command =>  'sed -i "s/15/10000/g" /etc/default/nginx;service nginx restart',
   path    =>  '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
   onlyif  =>  'test -e /etc/default/nginx',
